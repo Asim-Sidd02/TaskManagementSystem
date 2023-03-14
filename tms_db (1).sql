4edeee528f965ab94ab58f7e373a0dde84a03f1e
Asim Sidd <100421658+Asim-Sidd02@users.noreply.github.com>
2023-03-14 14:52:40 +0530

    Add files via upload

---
+++ tms_db (1).sql
@@ -0,0 +1,269 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 14, 2023 at 10:20 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.1.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `tms_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `project_list`
+--
+
+CREATE TABLE `project_list` (
+  `id` int(30) NOT NULL,
+  `name` varchar(200) NOT NULL,
+  `description` text NOT NULL,
+  `status` tinyint(2) NOT NULL,
+  `start_date` date NOT NULL,
+  `end_date` date NOT NULL,
+  `manager_id` int(30) NOT NULL,
+  `user_ids` text NOT NULL,
+  `date_created` datetime NOT NULL DEFAULT current_timestamp(),
+  `link` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `project_list`
+--
+
+INSERT INTO `project_list` (`id`, `name`, `description`, `status`, `start_date`, `end_date`, `manager_id`, `user_ids`, `date_created`, `link`) VALUES
+(3, 'AI Chat Bot', '&lt;b&gt;Faculty: &lt;/b&gt;Ravi', 5, '2022-12-13', '2022-12-15', 6, '7,10', '2023-01-16 14:36:01', 'https://docs.google.com/document/d/122dIB-7kQmZAkY7uyWh4seFyIsFAxEsHtoaYALDW_b4/edit'),
+(4, 'MagicMirror', '																																												', 5, '2022-12-13', '2022-12-30', 6, '12,11', '2023-01-16 14:38:35', ''),
+(5, 'Project Management System', '											', 0, '2022-12-30', '2023-01-23', 6, '7,10', '2023-01-16 14:39:25', ''),
+(6, 'TimeTable', '																						', 5, '2022-12-20', '2023-01-05', 9, '13', '2023-01-16 14:42:34', 'https://docs.google.com/document/d/1n_TCsq18d-fMoTGE_tEsrTe10WSes26Fmn1WLRL3VpU/edit?usp=sharing');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `system_settings`
+--
+
+CREATE TABLE `system_settings` (
+  `id` int(30) NOT NULL,
+  `name` text NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `contact` varchar(20) NOT NULL,
+  `address` text NOT NULL,
+  `cover_img` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `system_settings`
+--
+
+INSERT INTO `system_settings` (`id`, `name`, `email`, `contact`, `address`, `cover_img`) VALUES
+(1, 'Task Management System', 'info@sample.comm', '+6948 8542 623', '2102  Caldwell Road, Rochester, New York, 14608', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tasks`
+--
+
+CREATE TABLE `tasks` (
+  `task_id` int(11) NOT NULL,
+  `task_name` varchar(255) NOT NULL,
+  `start_date` date NOT NULL,
+  `end_date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `task_list`
+--
+
+CREATE TABLE `task_list` (
+  `id` int(30) NOT NULL,
+  `project_id` int(30) NOT NULL,
+  `task` varchar(200) NOT NULL,
+  `description` varchar(6) NOT NULL,
+  `Progress` int(4) NOT NULL,
+  `status` tinyint(4) NOT NULL,
+  `date_created` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `task_list`
+--
+
+INSERT INTO `task_list` (`id`, `project_id`, `task`, `description`, `Progress`, `status`, `date_created`) VALUES
+(5, 4, 'Documentation', '						', 100, 3, '2023-01-16 14:47:55'),
+(6, 4, 'Make UI', '						', 100, 3, '2023-01-16 14:49:04'),
+(7, 4, 'Feed Data', '						', 100, 3, '2023-01-16 14:49:22'),
+(8, 3, 'Documentation', '						', 100, 3, '2023-01-16 14:51:44'),
+(9, 3, 'Integrate API', '						', 100, 3, '2023-01-16 14:52:16'),
+(10, 3, 'Connect To Hardware Device', '						', 100, 3, '2023-01-16 14:57:41'),
+(11, 6, 'Documentation', '						', 100, 3, '2023-01-16 14:59:36'),
+(12, 5, 'Documentation', '						', 100, 3, '2023-01-16 15:00:04'),
+(13, 5, 'Make UI', '						', 100, 3, '2023-01-16 15:00:30'),
+(14, 5, 'Make Database', '						', 98, 3, '2023-01-16 15:00:56'),
+(15, 5, 'Make attendance system', '						', 100, 3, '2023-01-16 15:03:26'),
+(16, 5, 'Make Gantt Chart', '						', 48, 2, '2023-01-16 15:03:51'),
+(17, 5, 'Make Demonstration video', '						', 0, 1, '2023-01-16 15:04:12');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(30) NOT NULL,
+  `firstname` varchar(200) NOT NULL,
+  `lastname` varchar(200) NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `password` text NOT NULL,
+  `type` tinyint(1) NOT NULL DEFAULT 2 COMMENT '1 = admin, 2 = staff',
+  `avatar` text NOT NULL DEFAULT 'no-image-available.png',
+  `date_created` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `firstname`, `lastname`, `email`, `password`, `type`, `avatar`, `date_created`) VALUES
+(1, 'Administrator', '', 'admin@admin.com', '0192023a7bbd73250516f069df18b500', 1, 'no-image-available.png', '2020-11-26 10:57:04'),
+(6, 'Sumanth ', 'Pagadala', 'sumanth@gmail.com', '202cb962ac59075b964b07152d234b70', 2, 'no-image-available.png', '2023-01-16 14:32:34'),
+(7, 'Asim', 'Siddiqui', 'asim@gmail.com', '202cb962ac59075b964b07152d234b70', 3, 'no-image-available.png', '2023-01-16 14:33:35'),
+(8, 'Abha', 'Boob', 'abha@gmail.com', '202cb962ac59075b964b07152d234b70', 3, 'no-image-available.png', '2023-01-16 14:34:07'),
+(9, 'G', 'Vivek', 'vivek@gmail.com', '202cb962ac59075b964b07152d234b70', 2, 'no-image-available.png', '2023-01-16 14:34:36'),
+(10, 'Hamza', 'Khan', 'hamza@gmail.com', '202cb962ac59075b964b07152d234b70', 3, 'no-image-available.png', '2023-01-16 14:34:59'),
+(11, 'Toheed', 'Jamaal', 'toheed@gmail.com', '202cb962ac59075b964b07152d234b70', 3, 'no-image-available.png', '2023-01-16 14:35:32'),
+(12, 'Shifa', 'Naaz', 'shifa@gmail.com', '202cb962ac59075b964b07152d234b70', 3, 'no-image-available.png', '2023-01-16 14:37:28'),
+(13, 'Tanuj', 'T', 'tanuj@gmail.com', '202cb962ac59075b964b07152d234b70', 3, 'no-image-available.png', '2023-01-16 14:40:48'),
+(14, 'Maesh', 'M', 'maesh@gmail.com', '202cb962ac59075b964b07152d234b70', 3, 'no-image-available.png', '2023-01-16 14:43:40');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_productivity`
+--
+
+CREATE TABLE `user_productivity` (
+  `id` int(30) NOT NULL,
+  `project_id` int(30) NOT NULL,
+  `task_id` int(30) NOT NULL,
+  `comment` text NOT NULL,
+  `subject` varchar(200) NOT NULL,
+  `date` date NOT NULL,
+  `start_time` time NOT NULL,
+  `end_time` time NOT NULL,
+  `user_id` int(30) NOT NULL,
+  `time_rendered` float NOT NULL,
+  `date_created` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user_productivity`
+--
+
+INSERT INTO `user_productivity` (`id`, `project_id`, `task_id`, `comment`, `subject`, `date`, `start_time`, `end_time`, `user_id`, `time_rendered`, `date_created`) VALUES
+(1, 1, 1, '							&lt;p&gt;Sample Progress&lt;/p&gt;&lt;ul&gt;&lt;li&gt;Test 1&lt;/li&gt;&lt;li&gt;Test 2&lt;/li&gt;&lt;li&gt;Test 3&lt;/li&gt;&lt;/ul&gt;																			', 'Sample Progress', '2020-12-03', '08:00:00', '10:00:00', 1, 2, '2020-12-03 12:13:28'),
+(2, 1, 1, '							Sample Progress						', 'Sample Progress 2', '2020-12-03', '13:00:00', '14:00:00', 1, 1, '2020-12-03 13:48:28'),
+(3, 1, 2, '							Sample						', 'Test', '2020-12-03', '08:00:00', '09:00:00', 5, 1, '2020-12-03 13:57:22'),
+(4, 1, 2, 'asdasdasd', 'Sample Progress', '2020-12-02', '08:00:00', '10:00:00', 2, 2, '2020-12-03 14:36:30');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `project_list`
+--
+ALTER TABLE `project_list`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `system_settings`
+--
+ALTER TABLE `system_settings`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tasks`
+--
+ALTER TABLE `tasks`
+  ADD PRIMARY KEY (`task_id`);
+
+--
+-- Indexes for table `task_list`
+--
+ALTER TABLE `task_list`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user_productivity`
+--
+ALTER TABLE `user_productivity`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `project_list`
+--
+ALTER TABLE `project_list`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `system_settings`
+--
+ALTER TABLE `system_settings`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `tasks`
+--
+ALTER TABLE `tasks`
+  MODIFY `task_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `task_list`
+--
+ALTER TABLE `task_list`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT for table `user_productivity`
+--
+ALTER TABLE `user_productivity`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
